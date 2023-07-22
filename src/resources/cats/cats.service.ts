3b4ed10b2473d1dc8110d673654f35a0593e311f
Ahmet Berk TAŞ <63806963+Apperk274@users.noreply.github.com>
2023-07-22 22:56:09 +0300

    feat: Example usage of repositories

--- src/resources/cats/cats.service.ts
+++ src/resources/cats/cats.service.ts
@@ -1,26 +1,33 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCatDto } from './dto/create-cat.dto';
-import { UpdateCatDto } from './dto/update-cat.dto';
+import { Injectable } from '@nestjs/common'
+import { CreateCatDto } from './dto/create-cat.dto'
+import { UpdateCatDto } from './dto/update-cat.dto'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Cat } from 'src/resources/cats/entities/cat.entity'
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class CatsService {
-  create(createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+  constructor(
+    @InjectRepository(Cat)
+    private catsRepository: Repository<Cat>
+  ) {}
+  async create(createCatDto: CreateCatDto) {
+    await this.catsRepository.save(createCatDto)
   }
 
   findAll() {
-    return `This action returns all cats`;
+    return this.catsRepository.findAndCount()
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} cat`;
+    return `This action returns a #${id} cat`
   }
 
   update(id: number, updateCatDto: UpdateCatDto) {
-    return `This action updates a #${id} cat`;
+    return `This action updates a #${id} cat`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} cat`;
+    return `This action removes a #${id} cat`
   }
 }
